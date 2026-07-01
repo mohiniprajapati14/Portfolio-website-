6e2d3ffe9bff14c2a2c251364e51ebe23f12fa87
mohiniprajapati14 <jaiwalpriti448@gmail.com>
2026-07-01 17:50:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-document.querySelectorAll('nav a').forEach(a=>{
-  a.addEventListener('click',e=>{
-    e.preventDefault();
-    document.querySelector(a.getAttribute('href')).scrollIntoView({behavior:'smooth'});
+// scroll reveal
+const sections = document.querySelectorAll('.reveal');
+
+window.addEventListener('scroll', () => {
+  sections.forEach(sec => {
+    const top = sec.getBoundingClientRect().top;
+    if(top < window.innerHeight - 100){
+      sec.classList.add('show');
+    }
   });
 });
