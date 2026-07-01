7aeb7d4c2716374b4d7a80bf1f433b3da78fb728
mohiniprajapati14 <jaiwalpriti448@gmail.com>
2026-07-01 13:34:08 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("Portfolio website loaded");
+document.querySelectorAll('a[href^="#"]').forEach(a => {
+  a.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(a.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+const text = "Mohini Prajapati";
+let i = 0;
+
+function type() {
+  if (i < text.length) {
+    document.getElementById("typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(type, 120);
+  }
+}
+
+document.getElementById("typing").innerHTML = "";
+type();
+
+document.querySelector("form").addEventListener("submit", function(e){
+  e.preventDefault();
+  alert("Message sent successfully!");
+});
